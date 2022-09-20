cdf7b5a206885b0c87ce9d3e01e54c97bdf3df48
Varga <vargazsigmond02@gmail.com>
2022-09-20 13:57:25 +0200

    ember letrehoz

---
+++ src/hu/petrik/emberekoop/Main.java
@@ -0,0 +1,8 @@
+package hu.petrik.emberekoop;
+
+public class Main {
+    public static void main(String[] args) {
+        Ember human = new Ember("Gipsz Jakab", "2001-1-1","Budapest");
+        System.out.println(human);
+    }
+}
